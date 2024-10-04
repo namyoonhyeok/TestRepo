4091a5fbd3d15efe071a0991d104d709a15a9332
Yoon Hyeok <127106312+namyoonhyeok@users.noreply.github.com>
2024-10-04 23:34:51 +0900

    Add files via upload

---
+++ src/main/java/com/merging/branchify/BranchifyApplication.java
@@ -0,0 +1,13 @@
+package com.merging.branchify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merging/branchify/BranchifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merging.branchify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
